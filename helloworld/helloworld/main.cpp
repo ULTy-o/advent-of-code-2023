9a40eac818b55c3115460f5f14d5cd5bf26bfd61
ULTy <47011082+ULTy-o@users.noreply.github.com>
2023-11-30 00:44:57 -0500

    placeholder setup
    tested using xcode to compile c++ on mac, created repo to hold advent of code files

---
+++ helloworld/helloworld/main.cpp
@@ -0,0 +1,13 @@
+//
+//  main.cpp
+//  helloworld
+//
+//  Created by Michael Osvald on 2023-11-30.
+//
+
+#include <iostream>
+using namespace std;
+int main() {
+    // insert code here...
+    cout << "Hello, World!\n";
+}
